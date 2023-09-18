e5f1bea1cd95e20c4967486622dafe759d043b89
Red_Spash <red.spash.game@gmail.com>
2023-09-18 11:36:04 +0200

    Added HotBar game

---
+++ src/main/java/fr/red_spash/crazygames/game/games/hotbarspeed/HotBarSpeed.java
@@ -0,0 +1,120 @@
+package fr.red_spash.crazygames.game.games.hotbarspeed;
+
+import fr.red_spash.crazygames.game.games.hotblock.HotBarPreset;
+import fr.red_spash.crazygames.game.manager.PlayerData;
+import fr.red_spash.crazygames.game.models.Game;
+import fr.red_spash.crazygames.game.models.GameType;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.*;
+
+public class HotBarSpeed extends Game {
+
+    private final ArrayList<HotBarPreset> hotBarPresets;
+    private final HashMap<UUID,Integer> presetReached;
+    public static final int MAX_HOTBAR = 20;
+
+
+
+    public HotBarSpeed() {
+        super(GameType.HOTBAR_SPEED);
+
+        this.presetReached = new HashMap<>();
+        this.hotBarPresets = new ArrayList<>();
+        this.hotBarPresets.add(new HotBarPreset(Material.LIME_CONCRETE,Material.RED_CONCRETE));
+        this.hotBarPresets.add(new HotBarPreset(Material.LAVA_BUCKET,Material.WATER_BUCKET,Material.BRAIN_CORAL,Material.SEAGRASS,Material.KELP,Material.WET_SPONGE,Material.BUBBLE_CORAL_BLOCK,Material.CHERRY_BOAT,Material.COD,Material.TROPICAL_FISH));
+        this.hotBarPresets.add(new HotBarPreset(Material.IRON_CHESTPLATE,Material.FISHING_ROD,Material.DIAMOND_HOE,Material.GOLDEN_SHOVEL,Material.IRON_AXE,Material.NETHERITE_SWORD,Material.STONE_SHOVEL,Material.DIAMOND_AXE,Material.NETHERITE_PICKAXE));
+        this.hotBarPresets.add(new HotBarPreset(Material.GREEN_WOOL,Material.ACACIA_LEAVES,Material.AZALEA_LEAVES,Material.BIRCH_LEAVES,Material.JUNGLE_LEAVES,Material.MANGROVE_LEAVES,Material.DARK_OAK_LEAVES,Material.SPRUCE_LEAVES,Material.OAK_LEAVES));
+        this.hotBarPresets.add(new HotBarPreset(Material.GRASS_BLOCK,Material.NETHERRACK,Material.NETHER_QUARTZ_ORE,Material.PIGLIN_HEAD,Material.BASALT,Material.BLACKSTONE,Material.NETHER_BRICK,Material.CRIMSON_STEM,Material.NETHER_GOLD_ORE));
+        this.hotBarPresets.add(new HotBarPreset(Material.WHITE_CONCRETE_POWDER,Material.WHITE_WOOL,Material.BLACK_WOOL,Material.BLUE_WOOL,Material.BROWN_WOOL,Material.CYAN_WOOL,Material.GRAY_WOOL,Material.BLUE_WOOL,Material.GREEN_WOOL,Material.LIME_WOOL,Material.PINK_WOOL,Material.YELLOW_WOOL,Material.RED_WOOL));
+        this.hotBarPresets.add(new HotBarPreset(Material.RED_WOOL,Material.LIME_WOOL));
+        this.hotBarPresets.add(new HotBarPreset(Material.MAP,Material.PAPER));
+        this.hotBarPresets.add(new HotBarPreset(Material.DIRT,Material.GRASS_BLOCK));
+        this.hotBarPresets.add(new HotBarPreset(Material.STONE,Material.DEEPSLATE));
+        this.hotBarPresets.add(new HotBarPreset(Material.STONE,Material.COAL_ORE,Material.IRON_ORE,Material.GOLD_ORE,Material.DIAMOND_ORE,Material.COPPER_ORE,Material.EMERALD_ORE,Material.LAPIS_ORE,Material.REDSTONE_ORE));
+        this.hotBarPresets.add(new HotBarPreset(Material.GRASS,Material.ACACIA_SAPLING,Material.BIRCH_SAPLING,Material.CHERRY_SAPLING,Material.SPRUCE_SAPLING, Material.OAK_SAPLING,Material.DARK_OAK_SAPLING, Material.JUNGLE_SAPLING));
+        this.hotBarPresets.add(new HotBarPreset(Material.PURPLE_WOOL,Material.AMETHYST_BLOCK,Material.AMETHYST_CLUSTER,Material.AMETHYST_SHARD, Material.BUDDING_AMETHYST,Material.LARGE_AMETHYST_BUD,Material.MEDIUM_AMETHYST_BUD,Material.SMALL_AMETHYST_BUD));
+        this.hotBarPresets.add(new HotBarPreset(Material.DISC_FRAGMENT_5,Material.COCOA_BEANS,Material.BEETROOT_SEEDS,Material.MELON_SEEDS,Material.PUMPKIN_SEEDS,Material.WHEAT_SEEDS,Material.TORCHFLOWER_SEEDS));
+        this.hotBarPresets.add(new HotBarPreset(Material.TARGET,Material.CHEST,Material.CHEST_MINECART,Material.ENDER_CHEST, Material.TRAPPED_CHEST,Material.FURNACE,Material.DROPPER,Material.DISPENSER,Material.BLAST_FURNACE,Material.SMOKER));
+        this.hotBarPresets.add(new HotBarPreset(Material.RED_DYE,Material.REDSTONE,Material.REDSTONE_BLOCK,Material.PISTON,Material.DROPPER,Material.REPEATER,Material.REDSTONE_TORCH,Material.REDSTONE_LAMP,Material.TARGET,Material.BAMBOO_BUTTON,Material.POWERED_RAIL,Material.OBSERVER,Material.HOPPER));
+        this.hotBarPresets.add(new HotBarPreset(Material.CYAN_CONCRETE_POWDER,Material.PRISMARINE,Material.PRISMARINE_SLAB,Material.PRISMARINE_STAIRS,Material.PRISMARINE_WALL,Material.PRISMARINE_BRICKS,Material.PRISMARINE_BRICK_SLAB,Material.PRISMARINE_BRICK_STAIRS,Material.DARK_PRISMARINE,Material.DARK_PRISMARINE_SLAB,Material.DARK_PRISMARINE_STAIRS));
+        this.hotBarPresets.add(new HotBarPreset(Material.HONEY_BLOCK,Material.SEA_LANTERN,Material.OCHRE_FROGLIGHT,Material.PEARLESCENT_FROGLIGHT,Material.VERDANT_FROGLIGHT,Material.SHROOMLIGHT,Material.GLOWSTONE,Material.LANTERN,Material.SOUL_LANTERN,Material.TORCH,Material.SOUL_TORCH,Material.END_ROD));
+        this.hotBarPresets.add(new HotBarPreset(Material.MOSS_BLOCK,Material.FARMLAND,Material.ROOTED_DIRT,Material.COARSE_DIRT,Material.DIRT,Material.DIRT_PATH,Material.MYCELIUM,Material.GRASS_BLOCK,Material.PODZOL));
+        this.hotBarPresets.add(new HotBarPreset(Material.WHITE_CONCRETE_POWDER,Material.QUARTZ_SLAB,Material.QUARTZ_STAIRS,Material.QUARTZ_BLOCK,Material.SMOOTH_QUARTZ,Material.SMOOTH_QUARTZ_SLAB,Material.SMOOTH_QUARTZ_STAIRS,Material.QUARTZ_PILLAR,Material.QUARTZ_BRICKS,Material.CHISELED_QUARTZ_BLOCK));
+        this.hotBarPresets.add(new HotBarPreset(Material.MINECART,Material.COMMAND_BLOCK,Material.CHAIN_COMMAND_BLOCK,Material.REPEATING_COMMAND_BLOCK,Material.JIGSAW,Material.STRUCTURE_BLOCK,Material.STRUCTURE_VOID,Material.BARRIER,Material.LIGHT,Material.DEBUG_STICK,Material.COMMAND_BLOCK_MINECART));
+        this.hotBarPresets.add(new HotBarPreset(Material.WHITE_SHULKER_BOX,Material.WHITE_GLAZED_TERRACOTTA,Material.LIGHT_GRAY_GLAZED_TERRACOTTA,Material.GRAY_GLAZED_TERRACOTTA,Material.BLACK_GLAZED_TERRACOTTA,Material.BROWN_GLAZED_TERRACOTTA,Material.RED_GLAZED_TERRACOTTA,Material.ORANGE_GLAZED_TERRACOTTA,Material.YELLOW_GLAZED_TERRACOTTA,Material.LIME_GLAZED_TERRACOTTA,Material.GREEN_GLAZED_TERRACOTTA,Material.CYAN_GLAZED_TERRACOTTA,Material.BLUE_GLAZED_TERRACOTTA,Material.LIGHT_BLUE_GLAZED_TERRACOTTA,Material.PURPLE_GLAZED_TERRACOTTA,Material.PINK_GLAZED_TERRACOTTA,Material.MAGENTA_GLAZED_TERRACOTTA));
+        this.hotBarPresets.add(new HotBarPreset(Material.RED_DYE,Material.LIME_DYE));
+        this.hotBarPresets.add(new HotBarPreset(Material.LIME_DYE,Material.RED_DYE));
+        this.hotBarPresets.add(new HotBarPreset(Material.SPRUCE_PLANKS,Material.OAK_PLANKS));
+        this.hotBarPresets.add(new HotBarPreset(Material.DARK_OAK_LOG,Material.SPRUCE_LOG));
+        this.hotBarPresets.add(new HotBarPreset(Material.ITEM_FRAME,Material.GLOW_ITEM_FRAME));
+        this.hotBarPresets.add(new HotBarPreset(Material.SUSPICIOUS_GRAVEL,Material.GRAVEL));
+        this.hotBarPresets.add(new HotBarPreset(Material.SUSPICIOUS_SAND,Material.SAND));
+        this.hotBarPresets.add(new HotBarPreset(Material.GLASS_BOTTLE,Material.DRAGON_BREATH));
+        this.hotBarPresets.add(new HotBarPreset(Material.SPLASH_POTION,Material.POTION));
+        this.hotBarPresets.add(new HotBarPreset(Material.DETECTOR_RAIL,Material.RAIL));
+        this.hotBarPresets.add(new HotBarPreset(Material.SOUL_TORCH,Material.TORCH));
+        this.hotBarPresets.add(new HotBarPreset(Material.BAMBOO_CHEST_RAFT,Material.OAK_CHEST_BOAT));
+        this.hotBarPresets.add(new HotBarPreset(Material.ENCHANTED_GOLDEN_APPLE,Material.GOLDEN_APPLE));
+        this.hotBarPresets.add(new HotBarPreset(Material.DAMAGED_ANVIL,Material.ANVIL));
+    }
+
+    @Override
+    public void preStart(){
+        ArrayList<Location> otherSpawns = new ArrayList<>(super.getGameMap().getOtherLocations());
+        if(otherSpawns.size() >= super.getGameManager().getPlayerManager().getAlivePlayerData().size()){
+            for(Player p : Bukkit.getOnlinePlayers()){
+                PlayerData playerData = super.gameManager.getPlayerData(p.getUniqueId());
+                if(!playerData.isDead()){
+                    p.teleport(otherSpawns.remove(0));
+                    p.setWalkSpeed(0);
+                }
+            }
+        }
+
+        Collections.shuffle(this.hotBarPresets);
+        super.registerListener(new HotBarSpeedListener(this,this.hotBarPresets));
+    }
+
+
+    public Integer getPresetReached(UUID uuid) {
+        return presetReached.get(uuid);
+    }
+
+    @Override
+    public void startGame() {
+        for(Player p : Bukkit.getOnlinePlayers()){
+            PlayerData playerData = super.gameManager.getPlayerData(p.getUniqueId());
+            if(!playerData.isDead()){
+                this.setNextPreset(p);
+            }
+        }
+    }
+
+    public void setNextPreset(Player p) {
+        if(!presetReached.containsKey(p.getUniqueId())){
+            presetReached.put(p.getUniqueId(),-1);
+        }
+        presetReached.put(p.getUniqueId(),presetReached.get(p.getUniqueId())+1);
+        this.giveActualPreset(p);
+    }
+
+    public void giveActualPreset(Player p) {
+        p.getInventory().clear();
+        HotBarPreset hotBarPreset = this.hotBarPresets.get(this.presetReached.get(p.getUniqueId()));
+
+        hotBarPreset.giveRandomItems(p);
+    }
+
+    @Override
+    public List<String> updateScoreboard(Player p) {
+        int amount = 0;
+        if(presetReached.containsKey(p.getUniqueId())){
+            amount = this.presetReached.get(p.getUniqueId());
+        }
+        return List.of("Complétés: "+amount+"/"+HotBarSpeed.MAX_HOTBAR);
+    }
+}
---
+++ src/main/java/fr/red_spash/crazygames/game/games/hotbarspeed/HotBarSpeedListener.java
@@ -0,0 +1,116 @@
+package fr.red_spash.crazygames.game.games.hotbarspeed;
+
+import fr.red_spash.crazygames.game.games.hotblock.HotBarPreset;
+import fr.red_spash.crazygames.game.manager.GameStatus;
+import fr.red_spash.crazygames.game.manager.PlayerData;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+public class HotBarSpeedListener implements Listener {
+    private final HashMap<UUID, Integer> mistakes;
+    private final ArrayList<HotBarPreset> hotBarPresets;
+    private final HashMap<UUID,Long> cooldown;
+    private final HotBarSpeed hotBarSpeed;
+
+    public HotBarSpeedListener(HotBarSpeed hotBarSpeed, List<HotBarPreset> hotBarPresets) {
+        this.hotBarPresets = new ArrayList<>(hotBarPresets);
+        this.hotBarSpeed = hotBarSpeed;
+        this.cooldown = new HashMap<>();
+        this.mistakes = new HashMap<>();
+    }
+
+    @EventHandler
+    public void playerMoveEvent(PlayerMoveEvent e){
+        if(!e.getFrom().getWorld().getName().equals(this.hotBarSpeed.getGameMap().getWorld().getName()))return;
+        Player p = e.getPlayer();
+        PlayerData playerData = this.hotBarSpeed.getGameManager().getPlayerData(p.getUniqueId());
+        if(!playerData.isDead() && !playerData.isEliminated()){
+            if(e.getFrom().getBlock() != e.getTo().getBlock()){
+                Location to = e.getFrom();
+                to.setPitch(e.getTo().getPitch());
+                to.setYaw(e.getTo().getYaw());
+                e.setTo(to);
+            }
+        }
+    }
+
+    @EventHandler
+    public void liquidFlow(BlockFromToEvent e){
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void playerInteractEvent(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        PlayerData playerData = this.hotBarSpeed.getGameManager().getPlayerData(p.getUniqueId());
+        if(playerData.isDead() || playerData.isEliminated()) return;
+        if(this.hotBarSpeed.getGameManager().getActualGameStatus() != GameStatus.PLAYING)return;
+
+        Integer reachedPreset = this.hotBarSpeed.getPresetReached(p.getUniqueId());
+        if(reachedPreset == null)return;
+        e.setCancelled(true);
+        if(cooldown.containsKey(p.getUniqueId())){
+            if(cooldown.get(p.getUniqueId()) > System.currentTimeMillis()){
+                return;
+            }
+        }
+
+        cooldown.put(p.getUniqueId(),System.currentTimeMillis()+100);
+
+        HotBarPreset hotBarPreset = this.hotBarPresets.get(reachedPreset);
+        ItemStack itemStack = e.getItem();
+        if(itemStack != null){
+            if(itemStack.getType().equals(hotBarPreset.getRightMaterial())){
+                p.teleport(p.getLocation().add(0,1,0));
+                Location location = p.getLocation();
+                location.add(0,-1,0);
+                location.getBlock().setType(Material.SEA_LANTERN);
+                location.add(0,-1,0);
+                if(location.getBlock().getType() == Material.SEA_LANTERN){
+                    for(int x = -1; x<=1; x+=1){
+                        for(int z = -1; z<=1; z+=1){
+                            location.clone().add(x,0,z).getBlock().setType(Material.LIGHT);
+                        }
+                    }
+                    location.getBlock().setType(Material.WATER);
+                }
+                p.spigot().sendMessage(ChatMessageType.ACTION_BAR,new TextComponent("§a§lComplétés: "+(reachedPreset+1)+"/"+HotBarSpeed.MAX_HOTBAR));
+                if(reachedPreset+1 == HotBarSpeed.MAX_HOTBAR){
+                    String comment = "§a§lSANS FAUTE!";
+                    if(mistakes.containsKey(p.getUniqueId())){
+                        comment = "§c§lAVEC "+mistakes.get(p.getUniqueId())+" FAUTES!";
+                    }
+                    this.hotBarSpeed.getGameManager().getPlayerManager().qualifiedPlayer(p,comment);
+                    return;
+                }
+                p.getWorld().spawnParticle(Particle.VILLAGER_HAPPY,p.getLocation(),20,0.35,1,0.35);
+                p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP,1,1);
+                this.hotBarSpeed.setNextPreset(p);
+            }else{
+                if(!mistakes.containsKey(p.getUniqueId())){
+                    mistakes.put(p.getUniqueId(),0);
+                }
+                mistakes.put(p.getUniqueId(),mistakes.get(p.getUniqueId())+1);
+                p.sendHurtAnimation(10F);
+                p.getWorld().spawnParticle(Particle.FLAME,p.getLocation(),50,0.5,1,0.5,0.1);
+                p.playSound(p.getLocation(), Sound.BLOCK_ANVIL_PLACE,1,1);
+                p.getInventory().clear();
+                Bukkit.getScheduler().runTaskLater(this.hotBarSpeed.getGameManager().getMain(), () -> hotBarSpeed.giveActualPreset(p),20L);
+            }
+        }
+    }
+}
